978f556b577572c577b019660254dc4fdad2af09
Richard Hennigan <rhennigan@gmail.com>
2015-07-18 21:00:49 -0400

    added some options for exclusion patterns

--- FactorExpression2.m
+++ FactorExpression2.m
@@ -1,33 +1,49 @@
-(* Mathematica Package         *)
-(* Created by IntelliJ IDEA    *)
+(* Mathematica Package                                                            *)
+(* Created by IntelliJ IDEA                                                       *)
 
-(* :Title: FactorExpression2     *)
-(* :Context: FactorExpression2`  *)
-(* :Author: rhennigan            *)
-(* :Date: 7/16/2015              *)
+(* :Title: FactorExpression                                                       *)
+(* :Context: FactorExpression`                                                    *)
+(* :Author: Richard Hennigan                                                      *)
+(* :Date: 7/18/2015                                                               *)
 
-(* :Package Version: 1.0       *)
-(* :Mathematica Version:       *)
-(* :Copyright: (c) 2015 rhennigan *)
-(* :Keywords:                  *)
-(* :Discussion:                *)
+(* :Package Version: 0.1                                                          *)
+(* :Mathematica Version: 10.1.0  for Microsoft Windows (64-bit) (March 24, 2015)  *)
+(* :Copyright: (c) 2015 Richard Hennigan                                          *)
+(* :Keywords:                                                                     *)
+(* :Discussion:                                                                   *)
 
-BeginPackage["FactorExpression2`"];
+BeginPackage["FactorExpression`"];
+
+$DefaultExcludedForms =
+    {
+      _Random, _RandomChoice, _RandomColor, _RandomComplex, _RandomFunction, _RandomGraph, _RandomImage, _RandomInteger,
+      _RandomPermutation, _RandomPrime, _RandomReal, _RandomSample, _RandomSeed, _RandomVariate, _RandomWalkProcess,
+      Times[-1, _]
+    };
+
 (* Exported symbols added here with SymbolName::usage *)
 
 FactorExpression::usage = "Use this to make magic happen (maybe).";
 
 Begin["`Private`"]; (* Begin Private Context *)
 
-findRedundantExpressions[exp_, varCount_Integer] := Module[
+FactorExpression::depth = "The depth specification `1` is not a positive integer.";
+
+findRedundantExpressions[exp_, varCount_Integer, minDepth_Integer, excludedForms_List] := Module[
   {expressionCounts, mostRedundantExpressions},
-  expressionCounts = Tally[Cases[exp, Except[V[_Integer], _[___]], Infinity]];
-  mostRedundantExpressions = Cases[MaximalBy[expressionCounts, Last], {_, x_Integer /; x > 1}];
+  expressionCounts =
+      Tally[Cases[exp,
+        Except[Alternatives @@ Prepend[excludedForms, V[_Integer]], _[___]],
+        Infinity]];
+  mostRedundantExpressions =
+      MaximalBy[Cases[expressionCounts,
+        {subExp_ /; Depth[subExp] >= minDepth, x_Integer /; x > 1}
+      ], Last];
   If[mostRedundantExpressions =!= {},
     Module[{newVar, newVal},
       {newVar, newVal} = {V[varCount], mostRedundantExpressions[[1, 1]]};
       Sow[HeldSet[newVar, newVal]];
-      findRedundantExpressions[exp /. newVal -> newVar, varCount + 1]
+      findRedundantExpressions[exp /. newVal -> newVar, varCount + 1, minDepth, excludedForms]
     ],
     Sow[exp];
     varCount - 1
@@ -37,18 +53,32 @@ findRedundantExpressions[exp_, varCount_Integer] := Module[
 FactorExpression[exp_, opts : OptionsPattern[]] := Block[
   {$RecursionLimit = Infinity, $IterationLimit = Infinity},
   Module[
-    {output, varCount, assignments, localVariables, cexp, mexp, fexp},
+    {
+      output, minDepth, excludedForms,
+      varCount, assignments, localVariables,
+      cexp, mexp, fexp
+    },
+
     output = OptionValue["Output"];
-    {varCount, assignments} = Reap[findRedundantExpressions[exp, 1]];
+    minDepth = OptionValue["MinDepth"];
+    excludedForms = OptionValue["ExcludedForms"];
+
+    If[Not[IntegerQ[minDepth] && minDepth >= 1], Message[FactorExpression::depth, minDepth]];
+
+    {varCount, assignments} = Reap[findRedundantExpressions[exp, 1, minDepth, excludedForms]];
     localVariables = Symbol["V" <> ToString[#]]& /@ Range[varCount];
     cexp = First[HeldCompoundExpression @@@ assignments /. V[x_Integer] :> Symbol["V" <> ToString[x]]];
-    mexp = With[{localVariablesT = localVariables, cexpT = cexp}, Hold@Module[localVariablesT, cexpT]];
-    fexp = Evaluate[mexp] /. {HeldSet -> Set, HeldCompoundExpression -> CompoundExpression};
+    mexp = With[{localVariablesT = localVariables, cexpT = cexp}, Hold@Block[localVariablesT, cexpT]];
+    fexp = Evaluate[mexp] /. {HeldSet -> Set, HeldCompoundExpression -> CompoundExpression, Hold -> HoldForm};
+
     Switch[output,
       CompiledFunction, Module[
       {parameters = Union[Cases[exp, _Symbol, Infinity]]},
-      HeldCompile[{#, _Real}& /@ parameters, fexp] /. {
-        Hold[Module[vars_, modexp_]] :> Module[vars, modexp],
+      HeldCompile[{#, _Real}& /@ parameters, fexp (*),
+        Parallelization -> True,
+        RuntimeAttributes -> {Listable} *)
+      ] /. {
+        HoldForm[Block[vars_, modexp_]] :> Block[vars, modexp],
         HeldCompile -> Compile
       }
     ],
@@ -58,7 +88,9 @@ FactorExpression[exp_, opts : OptionsPattern[]] := Block[
 ];
 
 Options[FactorExpression] = {
-  "Output" -> CompiledFunction
+  "Output" -> Automatic,
+  "MinDepth" -> 1,
+  "ExcludedForms" -> $DefaultExcludedForms
 };
 
 End[]; (* End Private Context *)

021d043e135caa6b17d185809b8078f4613914a8
Richard Hennigan <rhennigan@gmail.com>
2015-08-12 19:52:04 -0400

    update

--- Metaprogramming.m
+++ Metaprogramming.m
@@ -15,39 +15,56 @@ Begin["`Private`"]; (* Begin Private Context *)
 
 (******************************************************************************)
 
+ClearAll[holdToString];
 SetAttributes[holdToString, {Listable}];
-holdToString[h_Hold] :=
-  StringTake[ToString[h], {6, -2}];
+holdToString[h_Hold] := StringTake[ToString[h], {6, -2}];
 
-symbolPositions[exp_] :=
-  Position[exp, Except[_[___]], Heads -> False];
+ClearAll[symbolPositions];
+symbolPositions[exp_] := Position[exp, _, Infinity, Heads -> True];
 
-extractHeldUnique[e_, p_] :=
-  DeleteDuplicates[Extract[e, p, Hold]];
+ClearAll[extractHeldUnique];
+extractHeldUnique[exp_] :=
+  exp // RightComposition[
+    DeleteDuplicates[Extract[#, symbolPositions[#], Hold]] &,
+    DeleteCases[#, h_Hold /; Depth[h] > 2] &,
+    Cases[#, h : Hold[s_Symbol] /; Context[s] =!= "System`"] &
+    ];
 
+ClearAll[iLocalReplacementRules];
 SetAttributes[iLocalReplacementRules, {HoldFirst}];
 iLocalReplacementRules[exp_] :=
-  Module[ {e, p, h, s, v},
-    e = Hold[exp];
-    p = symbolPositions[e];
-    h = extractHeldUnique[e, p];
-    s = holdToString[h];
-    v = Unique[s];
-    {h, v}
-  ];
+  Module[{e, h, s, v},
+   e = Hold[exp];
+   h = extractHeldUnique[e];
+   s = holdToString[h];
+   v = Unique[s];
+   {h, v}];
 
+ClearAll[localReplacementRules];
 SetAttributes[localReplacementRules, {HoldFirst}];
 localReplacementRules[exp_] :=
-  Module[ {h, v, r1, r2},
-    {h, v} = iLocalReplacementRules[exp];
-    r1 = Thread[h -> v] /. Hold -> HoldPattern;
-    r2 = Thread[v -> h];
-    {r1, r2}
-  ];
-localReplacementRules[exp_, "In"] :=
-  Thread[#1 -> #2] & @@ iLocalReplacementRules[exp] /. Hold -> HoldPattern;
-localReplacementRules[exp_, "Out"] :=
+  Module[{h, v, r1, r2},
+   {h, v} = iLocalReplacementRules[exp];
+   r1 = Thread[h -> v] /. Hold -> HoldPattern;
+   r2 = Thread[v -> h];
+   {r1, r2}];
+localReplacementRules[exp_, "In"] := 
+  Thread[#1 -> #2] & @@ iLocalReplacementRules[exp] /. 
+   Hold -> HoldPattern;
+localReplacementRules[exp_, "Out"] := 
   Thread[#2 -> #1] & @@ iLocalReplacementRules[exp];
+
+ClearAll[expandOnce];
+SetAttributes[expandOnce, {HoldAll}];
+expandOnce[e_] := Module[{v, r},
+   v = extractHeldUnique[e];
+   r = Flatten[v /. Hold[s_] :> DownValues[s]];
+   e //. r
+   ];
+
+ClearAll[expandAll];
+SetAttributes[expandAll, {HoldAll}];
+expandAll[e_] := FixedPoint[expandOnce, e];
 
 (******************************************************************************)
 
